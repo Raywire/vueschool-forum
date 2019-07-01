9f5c7c5ce889e20636816f92f2c3ead879cae4ec
Raywire <ryanwire@outlook.com>
2019-07-01 22:52:51 +0300

    sign users in and out

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import firebase from 'firebase/app'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import store from '@/store'
@@ -10,18 +10,17 @@ import AppDate from '@/components/AppDate'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
-  // Your web app's Firebase configuration
-const firebaseConfig = {
+
+// Initialize Firebase
+const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
-  appId: process.env.FIREBASE_APP_ID
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID
 }
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(config)
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
@@ -35,8 +34,5 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App },
-  beforeCreate () {
-    store.dispatch('fetchUser', {id: store.state.authId})
-  }
+  components: { App }
 })
